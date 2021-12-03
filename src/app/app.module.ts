6cdfd1d18fe74a74afb8fc1aefd3e248afcde8b3
Giedrius <kalniusg@gmail.com>
2021-12-04 01:47:25 +0200

    Created first dashboard and additional components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
 
-// import { MatButtonModule } from '@angular/material/button';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // MatButtonModule,
     DefaultModule,
   ],
   providers: [],
